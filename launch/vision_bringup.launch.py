10cf15700ad370fc06f5030c5bc60f3ec2e50d48
orion-gz <asdfg60842@gmail.com>
2026-06-07 13:59:58 +0900

    feat: add camera/robotarm macro

--- launch/vision_bringup.launch.py
+++ launch/vision_bringup.launch.py
@@ -1,20 +1,48 @@
 import os
+import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import Command
 
 def generate_launch_description():
-    # Gazebo 시뮬레이터 실행 (worlds 폴더의 sdf 파일 로드)
+    # Gazebo 시뮬레이터 실행 
     pkg_share = get_package_share_directory('robot_vision')
-    world_file = os.path.join(pkg_share, 'worlds', 'camera_test.sdf')
+    # ur3 + 카메라 xacro 파일 파싱
+    xacro_file = os.path.join(pkg_share, 'urdf', 'ur3_vision.urdf.xacro')
+    doc = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': Command(['xacro ', xacro_file])}
+    # 월드 모델 파일
+    world_file = os.path.join(pkg_share, 'worlds', 'yolo_test.sdf')
     
+    # 로봇 상태 퍼블리셔 (관절 위치와 tf 좌표계 계산해주는 노드)
+    rsp_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    # 가제보 실행
     gazebo_process = ExecuteProcess(
-        cmd=['ign', 'gazebo', world_file],
+        cmd=['ign', 'gazebo', world_file, '-r'],
         output='screen'
     )
 
-    # ROS-Gazebo 브릿지 노드 실행 (단방향 통신)
+    # 가제보에 로봇 소환
+    spawn_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'ur3_vision',
+            '-topic', 'robot_description',
+            '-x', '0', '-y', '0', '-z', '0.0'
+        ],
+        output='screen'
+    )
+
+    # ROS-Gazebo 브릿지 노드 실행 (카메라 데이터 통신)
     bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -33,7 +61,9 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        rsp_node,
         gazebo_process,
+        spawn_node,
         bridge_node,
         yolo_node
     ])
